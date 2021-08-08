0aed200a721c5812841639ee7d8ab12effc7547e
Jolinbaby <1292155168@qq.com>
2021-08-08 15:50:57 +0800

    20210808 By:Jolin

---
+++ Assets/Scripts/TipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipController : MonoBehaviour
+{
+    //public float displayTime = 2.0f;
+    private GameObject okButton;
+    private GameObject tip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject UIcanvas = GameObject.Find("TargetCanvas");
+        tip = UIcanvas.transform.Find("Tip").gameObject;
+        okButton= UIcanvas.transform.Find("okButton").gameObject;
+        okButton.GetComponent<Button>().onClick.AddListener(Click);
+    }
+
+    // Update is called once per frame
+    void Click()
+    {
+        tip.SetActive(false);
+        okButton.SetActive(false);
+    }
+}
